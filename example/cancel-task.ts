1bfb404f7f06ba0074dd4212bae4b44dd35df0b1
share121 <me@s121.top>
2026-02-27 19:59:20 +0800

    docs: 修改 example

--- example/cancel-task.ts
+++ example/cancel-task.ts
@@ -2,11 +2,16 @@ import { prefetch } from '..'
 import { join, resolve } from 'node:path'
 import { promises as fs } from 'node:fs'
 
-const URL =
-  'https://software.download.prss.microsoft.com/dbazure/Win11_25H2_Pro_Chinese_Simplified_x64.iso?t=40a41c61-f324-4b94-80ed-c6f7db5e591a&P1=1772243337&P2=601&P3=2&P4=BfXe6ch%2bgL6laiXUtKBlSpNSZ02ToktN6CtImJoYs%2bzsOIc9UQBCEeiC4nFNEmmFzNDmbUoTxWx4APJE7nbgFYEsdqiBVspvJSukOlOcXPJLpK%2fwQWeVxaCzgItfXV52UkEXU7NENvtXBZXUGGrAHUrjETQ65pQ7f31Qrbj4Gm90gZaUrFH30nYUatGLGC2db2CI0zfLVz8NACDKmbtgKLvLgpk2ZF%2fuSvGFB7gA5%2bl7qavaQqUwfE6MKJJSow8tdpXdv4gyQq2Ohk6L1qYqkaJIv0iGfxgjuAzhNjHGRiffNICp1bNcm%2bfBHbQij%2fPjioL0eoIfV3xBaNWa15br0g%3d%3d'
+const URL = 'https://mirrors.tuna.tsinghua.edu.cn/archlinux/iso/2026.02.01/archlinux-x86_64.iso'
 
 async function main() {
-  const task = await prefetch(URL, { proxy: 'no' })
+  const task = await prefetch(URL, {
+    proxy: 'no',
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36 Edg/145.0.0.0',
+    },
+  })
   setTimeout(() => {
     task.cancel()
     console.log('Download canceled')
